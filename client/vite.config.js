741fd2f795cc2a593b8cac822fd96292d943c8cf
Drew-Andersen <andrewtandersen@gmail.com>
2026-06-04 13:34:54 -0400

    Added the client side folders and made sure it worked

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      // And request starting with /api get forwarded straight to Express server
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+  }
 })
