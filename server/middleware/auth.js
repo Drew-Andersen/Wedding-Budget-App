4e15d9384c4367577d3bc199adbc03f0699572c2
Drew-Andersen <andrewtandersen@gmail.com>
2026-06-30 14:13:29 -0400

    Added some console logs for testing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,19 @@
 const jwt = require("jsonwebtoken")
 
 function requireAuth(req, res, next) {
+    console.log("Origin:", req.headers.origin)
+    console.log("Cookies:", req.cookies)
+    console.log("Cookie header:", req.headers.cookie)
+
     const token = req.cookies?.token || req.headers.authorization?.split(' ')[1]
-    if(!token) return res.status(401).json({ error: "Not authorized" })
+    if(!token) {
+        console.log("NO TOKEN RECEIVED")
+        return res.status(401).json({ error: "Not authorized" })
+    }
 
         try {
             req.user = jwt.verify(token, process.env.JWT_SECRET)
+            console.log("Authenticated:", req.user.username)
             next()
         } catch {
             res.clearCookie("token")
